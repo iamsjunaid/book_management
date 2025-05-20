2ca554f76d663222707d8081a5adc3115eb74503
Junaid Syed <sjunaid626@gmail.com>
2025-05-20 22:14:24 +0530

    feat: add ToastContainer for notifications in the main application

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import App from './App.tsx';
 import './index.css';
 
 const queryClient = new QueryClient();
@@ -12,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
+        <ToastContainer position="top-right" autoClose={3000} />
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
