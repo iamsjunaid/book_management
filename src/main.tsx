27837ff31d76a95692f90d133886b92c6b532234
Junaid Syed <sjunaid626@gmail.com>
2025-05-20 21:46:46 +0530

    fix: uncomment React.StrictMode in main.tsx for improved development experience

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,11 @@ import './index.css';
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </BrowserRouter>
-  // {/* </React.StrictMode> */}
+  </React.StrictMode>
 );
