10dc9b11dfaaecc16062d405eb07f45e3a14e618
Junaid Syed <sjunaid626@gmail.com>
2025-05-21 13:52:27 +0530

    feat: add background color to App component for improved aesthetics

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gray-50">
       {/* <TestApi /> */}
       <Dashboard />
     </div>
